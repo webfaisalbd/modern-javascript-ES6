affecd9a54113f06587af3a57ff615fdea694e81
unknown <web.faisal.bd@gmail.com>
2021-12-10 19:52:45 +0600

    arrow function syntax related issue

---
+++ arrowFunction.js
@@ -0,0 +1,16 @@
+// normal function 
+function number(a,b){
+    return a+b;
+}
+console.log(number(2,5));
+
+let sum = (x) => x+x;
+console.log(sum(10));
+
+let add = x => x+15;
+console.log("addition value is: "+ add(5) + ". And that is the answer");
+
+let mul = (x,y) => x+y;
+console.log(mul(2,4));
+
+
