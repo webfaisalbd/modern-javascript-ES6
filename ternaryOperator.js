3b910b5afef0f80f4565bd10384348061aacd3a8
unknown <web.faisal.bd@gmail.com>
2021-12-10 21:15:02 +0600

    ternaryOperator

---
+++ ternaryOperator.js
@@ -0,0 +1,12 @@
+// var isLogedIn= true;
+// var access= isLogedIn ? true : false;
+// console.log(access);
+
+// var isLogedIn= true;
+// var access= isLogedIn;
+// console.log(access);
+
+
+var age= 11;
+var type= (age>18) ? "adult" : (age<10) ? "child" : "young";
+console.log(type);
